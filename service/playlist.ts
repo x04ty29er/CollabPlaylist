20d8b77db204bb5952ac9c2806ff2a704c61fcd2
Blu <x04ty29er@gmail.com>
2015-09-30 21:54:32 -0700

    changed Playlist to use a map and indexing instead of just indexing. lets us test if we have a url already added or not

--- service/playlist.ts
+++ service/playlist.ts
@@ -1,41 +1,55 @@
 export default class Playlist {
-    private urlList: string[];
+    private urlList: { [url: string] : boolean };
+    private positionMap: string[];
+    private listSize: number; // Kept in sync with urlList size
     private position: number;
 
     constructor(startPos: number = 0) {
         this.position = startPos;
-        this.urlList = [];
+        this.listSize = 0;
+        this.urlList = {};
+        this.positionMap = [];
     }
 
-    public get all(): string[] {
-        return this.urlList;
+    public get all(): any {
+        return this.positionMap;
     }
 
     public get current(): string {
-        return this.urlList[this.position];
+        return this.positionMap[this.position];
     }
 
     public get next(): string {
-        this.position = (this.position + 1) % this.urlList.length;
-        return this.urlList[this.position];
+        this.position = (this.position + 1) % this.listSize;
+        return this.positionMap[this.position];
     }
 
     public get previous(): string {
-        this.position = (this.position - 1) % this.urlList.length;
-        return this.urlList[this.position];
-    }
-
-    public shuffle(): void {
-        // Shuffle List
+        this.position = (this.position - 1) % this.listSize;
+        return this.positionMap[this.position];
     }
     
     // Returns new size of list
     public addSong(url: string): number {
-        return this.urlList.push(url);
+        if (this.urlList[url]) {
+            return this.listSize;
+        }
+
+        this.listSize++;
+        this.urlList[url] = true;
+        this.positionMap.push(url);
+        return this.listSize;
     }
     
     // Returns removed Urls
     public removeSong(index: number = this.position): string[] {
-        return this.urlList.splice(index, 1);
+        var urlToRemove: string = this.positionMap[index];
+        if (urlToRemove && this.urlList[urlToRemove]) {
+            --this.listSize;
+            delete this.urlList[urlToRemove];
+            return this.positionMap.splice(index, 1);
+        }
+        
+        return [];
     }
 }
